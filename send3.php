94630e032092d5353d2327a41b88ae8b9aea7e9a
Maks <38716585+Melifaro00@users.noreply.github.com>
2018-05-16 17:59:11 +0700

    Add files via upload

--- send3.php
+++ send3.php
@@ -2,26 +2,28 @@
     <head>
         <title>Форма заявки с сайта</title>
     </head>
-<body>    
-    <?php //проверяем, существуют ли переменные в массиве POST
-        if(!isset($_POST['fio']) and !isset($_POST['email'])){
-    ?> 
-    <div style="width:400px;">
-    <form action="send3.php" method="post">
-        
-        <div style="min-width:130px; float:left;"> 
+<body>  
+<style>
+    .titl{
+        min-width:130px; 
+        float:left;
+    }
+</style>
+<div style="width:400px;">
+    <form action="send3.php" method="post">        
+        <div class="titl"> 
             <strong>Имя:</strong>
         </div>
         <div>
             <input type="text" name="fio" MINLENGTH="3" MAXLENGTH="40" placeholder="Укажите ФИО" required><br><br>
         </div>
-        <div style="min-width:130px; float:left;"> 
+        <div class="titl"> 
             <strong>E-mail:</strong>
         </div>
         <div>
             <input type="text" name="email" placeholder="Укажите e-mail" required><br><br>
         </div>
-        <div style="min-width:130px; float:left;"> 
+        <div class="titl"> 
             <strong>Текст вопроса:</strong>
         </div>
         <div>
@@ -30,18 +32,32 @@
         </div><br><br>
         <center><input type="submit" value="Отправить"></center>
     </form> 
-    </div>
-    <?php
-        } else {
-        //показываем форму
-        $fio = $_POST['fio'];
-        $email = $_POST['email'];
-        $message = $_POST['message'];           
+</div>
+<?php            
+    $fio = $_POST['fio'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];           
+    
+    // Генерация пароля
+    $chars="abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ-_"; // Допустимые символы, в пароле. 
+    $max=10; // Количество символов в пароле.         
+    $size=StrLen($chars)-1; // Определяем количество символов в $chars
+    $password=null; // Определяем пустую переменную, в которую и будем записывать символы. 
+    
+    // Создаём пароль. 
+    while($max--) 
+    $password.=$chars[rand(0,$size)];       
+    //echo "<left> Сгенерированный пароль: <font face=verdana color=red size=4> <b>".$password."</b><br><br></font> </left>"; 
 
     //mail("на какой адрес отправить", "тема письма", "Сообщение (тело письма)","From: с какого email отправляется письмо \r\n");
-    if (mail("Melifaro00@yandex.ru", "Заявка с сайта", 
-              "ФИО:".$fio.". E-mail: ".$email ,"From: Melifaro12@mail.ru \r\n"))
-        {             
+    if (mail("Melifaro00@yandex.ru", 
+            "Тема: Заявка с сайта",  
+            "<br>ФИО: ".$fio.
+            "<br>E-mail: ".$email.
+            "<br>Сообщение".$message.
+            "<br>Пароль:".$password ,
+            "From: Melifaro12@mail.ru \r\n"))
+    {             
         // Проверки
         // 1. функция преобразует все символы
         $fio = htmlspecialchars($fio);    
@@ -55,75 +71,9 @@
         // 3. удалим пробелы с начала и конца строки
         $fio = trim($fio); 
         $email = trim($email);
-        $message = trim($message);
-
-        // запись в массив
-        $arr[1] = $fio; 
-        $arr[2] = $email; 
-        $arr[3] = $message;        
-               
-        echo "<b>Успешно отправлено:</b> <br>";
-        echo $arr[1]; echo "<br>";
-        echo $arr[2]; echo "<br>";
-        echo $arr[3]; echo "<br>";
-        echo "<br>";
-        
-        // Генерация пароля
-        // Символы, которые будут использоваться в пароле. 
-        $chars="qazxswedcvfrtgbnhyujmkiolp1234567890QAZXSWEDCVFRTGBNHYUJMKIOLP"; 
-        // Количество символов в пароле. 
-        $max=10; 
-        // Определяем количество символов в $chars 
-        $size=StrLen($chars)-1; 
-        // Определяем пустую переменную, в которую и будем записывать символы. 
-        $password=null; 
-        // Создаём пароль. 
-        while($max--) 
-        $password.=$chars[rand(0,$size)]; 
-        // Выводим созданный пароль. 
-        echo "
-            <left> Сгенерированный пароль: 
-                <font face=verdana color=red size=4>
-                    <b>".$password."</b><br><br>
-                </font>            
-            </left>"; 
-        $strarr = serialize($arr); 
-        echo "<b>Cериализованный массив:</b>";
-        echo $strarr."<br />";         
-        echo "<br /> <b>Cериализованный закодированный массив:</b><br />";
-        echo base64_encode($strarr);
-
-        echo "<br /><br /> <b>Массив JSON:</b>"; 
-        $jarr=json_encode($arr, JSON_UNESCAPED_UNICODE);        
-        
-        // Открыть текстовый файл ,записать переменные в файл, 
-        // открыть файл для чтения и прочитать строку, закрыть.
-        $bd = fopen("bdtest.json", "w"); 
-        fwrite($bd, $jarr);
-        $bd = fopen("bdtest.json", "r");    
-        echo fgets($bd);    
-
-        // Открыть текстовый файл ,записать переменные в файл, 
-        // открыть файл для чтения и прочитать строку, закрыть.
-        $f = fopen("textfile.txt", "w");
-        
-        echo "<br><br> <b>Записано в файл: textfile.html </b><br>";
-        
-        fwrite($f, " <b>Ф.И.О:</b> ".$fio." <br>" ); 
-        fwrite($f, " <b>Email:</b> ".$email." <br>"); 
-        fwrite($f, " <b>Сообщение:</b> ".$message." <br>");         
-        fwrite($f, " <b>Пароль:</b> ".$password);                 
-
-        $f = fopen("textfile.html", "r");    
-        echo fgets($f);  
-
-        fclose($f);
-        fclose($bd);    
-        } 
-    else { echo "При отправке сообщения возникли ошибки"; }
-    }
+        $message = trim($message);             
+    } 
 ?>
-
 <style>
     .del { display: none; }
     .del:not(:checked) + label + * { display: none; } 
@@ -141,39 +91,6 @@
     background: #e36443;
     }*/
 </style> </br>
-<ul>
-    <li><a href="/FAQ.php">вернуться к справке</a></li>
-    <li><a href="/textfile.html">Посмотреть textfile.html</a></li>
-    <li><a href="/bdtest.json">Посмотреть bdtest.json</a></li>
-</ul>
-
-
-<?php
-// эксперимент - выборка элемента из json
-//$jsonString = '
-//{                                         
-// "orderID": 123,                       
-//  "shopperName": "Ваня",            
-//  "shopperEmail": "Vano@example.com",
-//  "contents": [                           
-//    {                                     
-//      "productID": 34,                    
-//      "productName": "Товар",       
-//      "quantity": 2                      
-//    },                                    
-//    {                                     
-//      "productID": 56,                    
-//      "productName": "Супер",
-//      "quantity": 3                       
-//    }                                     
-//  ],                                      
-//  "orderCompleted": true                  
-//}                                         
-//';
-//$cart = json_decode( $jsonString );
-//echo $cart->shopperEmail . "<br>";
-//echo $cart->contents[1]->productName . "<br>";
-
-?>
+<a href="/index.php">Вернуться к справке</a>
 </body>
 </html>
