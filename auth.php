da888e813c36bc9315077c98b083be7b350c26b7
Maks <38716585+Melifaro00@users.noreply.github.com>
2018-05-17 13:03:12 +0700

    Add files via upload

--- auth.php
+++ auth.php
@@ -1,76 +1,103 @@
-<html xmlns="http://www.w3.org/1999/xhtml"><head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<title>Untitled Document</title>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <title>Untitled Document</title>
+    <style>
+        .del { display: none; }
+        .del:not(:checked) + label + * { display: none; } 
+        /* вид CSS кнопки */
+        .del:not(:checked) + label,
+        .del:checked + label {
+        display: inline-block;
+        padding: 2px 10px; 
+        }
+        /*.del:checked + label */
+    </style> 
 </head>
 <body>
-<style>
-.del { display: none; }
-.del:not(:checked) + label + * { display: none; } 
-/* вид CSS кнопки */
-.del:not(:checked) + label,
-.del:checked + label {
-  display: inline-block;
-  padding: 2px 10px; 
-}
-/*.del:checked + label */
-</style> 
-<h2>Вход выполнен</h2>
-<?php session_start(); $login = $_SESSION['login'];?>
-<?php echo "Привет, $login" ?> <br><br>
-
-<button><a href="/send3.php">Задать вопрос</a></button>
-<button><a href="/index.php">Выйти</a></button>
-<!--button><a href="/register.php">Зарегистрироваться</a></button-->
-<hr>
-
-<input type="checkbox" id="raz" class="del"> 
-<label for="raz" class="del">
-    1. Как мне установить XAMPP?
-</label>
-<div>
-    <p>Выберите ващу операционную систему и 32 или 64 битную версию.</p>
-    <p>Измените разрешения для установщика</p>        
-    <p>chmod 755 xampp-linux-*-installer.run</p>        
-    <p>Запустите установщик</p>        
-    <p>sudo ./xampp-linux-*-installer.run</p>        
-    <p>Вот и всё. XAMPP теперь установлен под каталогом /opt/lampp.</p>    
-</div><br>
-
-<input type="checkbox" id="raz2" class="del"> 
-    <label for="raz2" class="del">
-        2. Как мне запустить XAMPP??
+<?php 
+    session_start(); 
+    $login = $_SESSION['login'];
+    $auth = $_SESSION['auth'];
+    //echo "login="; echo $_SESSION['login'];
+    //echo "auth="; echo $_SESSION['auth'];
+    if ($_SESSION["auth"] == 1)
+    {
+?>
+    <h2>Вход выполнен</h2>
+    <?php echo "Привет, $login" ?> <br><br>
+    <button><a href="/send3.php">Задать вопрос</a></button>
+    <form method="POST">        
+        <input name="exit" type="submit" value="Выйти"></p>  
+    </form>
+    <?php // Если кнопка нажата
+        if( isset( $_POST['exit'] ) )
+        {   session_destroy();
+        $auth = 0;   
+        echo $auth;
+        header ('Location: index.php');  // перенаправление на нужную страницу
+        exit();      
+    }
+    ?>    
+    <hr>
+    <input type="checkbox" id="raz" class="del"> 
+    <label for="raz" class="del">
+        1. Как мне установить XAMPP?
     </label>
-<div>
-    <p>Starting XAMPP 1.8.2...</p>
-    <p>LAMPP: Starting Apache...</p>
-    <p>LAMPP: Starting MySQL...</p>
-    <p>LAMPP started.</p>
-    <p>Ready. Apache and MySQL are running.</p>
-    <p>Если вы получили сообщение об ошибке посетите страницы нашего сообщества для помощи.</p>
-    <p>Кстати, заметьте что есть графический инструмент который вы можете использовать чтобы легко управлятся с вашими серверами. Вы можете запустить этот инструмент с помощью следующих команд:</p>
-    <p>cd /opt/lampp</p>
-    <p>sudo ./manager-linux.run (or manager-linux-x64.run)</p>
-</div><br>
+    <div>
+        <p>Выберите ващу операционную систему и 32 или 64 битную версию.</p>
+        <p>Измените разрешения для установщика</p>        
+        <p>chmod 755 xampp-linux-*-installer.run</p>        
+        <p>Запустите установщик</p>        
+        <p>sudo ./xampp-linux-*-installer.run</p>        
+        <p>Вот и всё. XAMPP теперь установлен под каталогом /opt/lampp.</p>    
+    </div><br>
 
-<input type="checkbox" id="raz3" class="del"> 
-    <label for="raz3" class="del">
-        3. Как я мне остановить XAMPP?
-    </label>
-<div>
-    <p> Для остановки XAMPP просто вызовите команду:</p>
-    <p>sudo /opt/lampp/lampp stop            </p>
-    <p>Теперь вы должны видить чтото вроде этого на вашем экране:</p>
-    <p>Stopping XAMPP 1.8.2...</p>
-    <p>LAMPP: Stopping Apache...  </p>          
-    <p>LAMPP: Stopping MySQL...       </p>     
-    <p>LAMPP stopped.            </p>
-    <p>Если вы получили сообщение об ошибке посетите страницы нашего сообщества для помощи.</p>
-    <p>Кстати, заметьте что есть графический инструмент который вы можете использовать чтобы легко запустить или остановить ваши сервера. Вы можете запустить этот инструмент с помощью следующих команд:</p>
-    <p>cd /opt/lampp</p> 
-    <p>sudo ./manager-linux.run (or manager-linux-x64.run)</p>
-</div><br>
-<br>
-<hr>
-<br><br>
+    <input type="checkbox" id="raz2" class="del"> 
+        <label for="raz2" class="del">
+            2. Как мне запустить XAMPP??
+        </label>
+    <div>
+        <p>Starting XAMPP 1.8.2...</p>
+        <p>LAMPP: Starting Apache...</p>
+        <p>LAMPP: Starting MySQL...</p>
+        <p>LAMPP started.</p>
+        <p>Ready. Apache and MySQL are running.</p>
+        <p>Если вы получили сообщение об ошибке посетите страницы нашего сообщества для помощи.</p>
+        <p>Кстати, заметьте что есть графический инструмент который вы можете использовать чтобы легко управлятся с вашими серверами. Вы можете запустить этот инструмент с помощью следующих команд:</p>
+        <p>cd /opt/lampp</p>
+        <p>sudo ./manager-linux.run (or manager-linux-x64.run)</p>
+    </div><br>
+
+    <input type="checkbox" id="raz3" class="del"> 
+        <label for="raz3" class="del">
+            3. Как я мне остановить XAMPP?
+        </label>
+    <div>
+        <p> Для остановки XAMPP просто вызовите команду:</p>
+        <p>sudo /opt/lampp/lampp stop            </p>
+        <p>Теперь вы должны видить чтото вроде этого на вашем экране:</p>
+        <p>Stopping XAMPP 1.8.2...</p>
+        <p>LAMPP: Stopping Apache...  </p>          
+        <p>LAMPP: Stopping MySQL...       </p>     
+        <p>LAMPP stopped.            </p>
+        <p>Если вы получили сообщение об ошибке посетите страницы нашего сообщества для помощи.</p>
+        <p>Кстати, заметьте что есть графический инструмент который вы можете использовать чтобы легко запустить или остановить ваши сервера. Вы можете запустить этот инструмент с помощью следующих команд:</p>
+        <p>cd /opt/lampp</p> 
+        <p>sudo ./manager-linux.run (or manager-linux-x64.run)</p>
+    </div><br>
+    <br>
+    <hr>
+    <br><br>
+    <?php 
+    }
+    else
+    {
+        header ('Location: index.php');  // перенаправление на нужную страницу
+        exit(); 
+    }
+?>
+
+
 </body>
 </html>
--- login.php
+++ login.php
@@ -10,6 +10,7 @@
     <input type="password" name="pass" placeholder="Пароль"><br><br>
     <input type="submit" name="submit" placeholder="Войти"><br>
 </form>
+
 <?php 
     // 1|user111|Y0dGemN6RXhNUT09|2|user222|Y0dGemN6SXlNZz09|3|user333|Y0dGemN6TXpNdz09|4|user444|Y0dGemN6UTBOQT09
     //  1|user111|pass111|2|user222|pass222|3|user333|pass333       
@@ -32,13 +33,15 @@ if (isset($_POST['submit']))
         
         //Шаг2 LOGIN    
         if( $login == $data[$z])
-        {   
+        {   $logtrue=true;
+
             $z=$z+1;            
             $data[$z]=base64_decode(base64_decode($data[$z])); // взяли пароль из базы и раскодировали            
             if( $pass == $data[$z])      // Сравнили с введенным с клавиатуры
-            {   
+            {   $passtrue=true; 
                 session_start();
-                $_SESSION['login'] = $login;  
+                $_SESSION['auth'] = 1;
+                $_SESSION['login'] = $login;                
                 header ('Location: auth.php');  // перенаправление на нужную страницу
                 exit();                          
             }            
